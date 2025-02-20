1899ac441119956dbe3bdf66cb350803f455c058
simpleplant999 <cedriclabrador@gmail.com>
2025-02-20 20:51:51 +0800

    update server

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,19 @@ import express, { Express } from 'express'
 import mongoose from 'mongoose'
 import driverScheduleRouter from './routes/driver-schedules'
 import cors from 'cors'
+import fs from 'fs'
+import https from 'https'
 const app: Express = express();
 const port = process.env.PORT || 3001;
 
+const sslOptions = {
+  key: fs.readFileSync('/etc/letsencrypt/live/ydrive-api.com/privkey.pem'),
+  cert: fs.readFileSync('/etc/letsencrypt/live/ydrive-api.com/fullchain.pem'),
+};
+
 app.use(express.json());
 app.use(cors())
+
 
 const mongoURI: string =  "mongodb+srv://ydriveappdb:BV1J0ow6mlLq8fPy@driverschedule.fxave.mongodb.net/";
 
@@ -18,6 +26,10 @@ mongoose.connect(mongoURI).then(()=>{
 
 app.use('/driver-schedules/', driverScheduleRouter)
 
-app.listen(port, ()=>{
-  console.log(`Server Running on Port ${port}`)
-})
+https.createServer(sslOptions, app).listen(port, () => {
+  console.log(`Server Running on HTTPS Port ${port}`);
+});
+
+// app.listen(port, ()=>{
+//   console.log(`Server Running on Port ${port}`)
+// })
